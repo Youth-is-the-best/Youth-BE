beb7e19ec92a7d397aa38689c00d7c4b741361d8
cherrynniii <chaelin20760@gmail.com>
2024-07-20 17:25:37 +0900

    feat: urls.py 회원가입 추가, settings.py 수정

---
+++ whew-are-you-BE/users/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import *
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
+
+urlpatterns = [
+    path("join/", RegisterView.as_view()),      # 회원가입
+]
