281e1ef21b88c8b32cd31e326b879659b874be3c
cherrynniii <chaelin20760@gmail.com>
2024-07-20 17:19:54 +0900

    feat: 회원가입 뷰 구현

--- whew-are-you-BE/users/views.py
+++ whew-are-you-BE/users/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from rest_framework_simplejwt.serializers import RefreshToken
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from .serializers import *
+
+# 회원가입 뷰
+class RegisterView(APIView):
+
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+
+        if serializer.is_valid(raise_exception=True):
+            user = serializer.save(request)
+            token = RefreshToken.for_user(user)
+            refresh_token = str(token)
+            access_token = str(token.access_token)
+            res = Response(
+                {
+                    "user": serializer.data,
+                    "message": "register success",
+                    "token": {
+                        "access_token": access_token,
+                        "refresh_token": refresh_token,
+                    },
+                },
+                status=status.HTTP_201_CREATED,
+            )
+            return res
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
