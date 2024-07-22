0c4365e685ccc33bb25515bb3f74b1c538d0328e
dylan <nyskyline7@gmail.com>
2024-07-22 20:58:10 +0900

    feat: 랜덤 인증코드 생성, 새 Verif모델로 저장

--- whew-are-you-BE/users/views.py
+++ whew-are-you-BE/users/views.py
@@ -7,6 +7,8 @@ from django.contrib.auth import logout
 
 from .serializers import *
 from .sendmail import send_email
+import random
+from .models import Verif
 
 
 # 회원가입 뷰
@@ -75,6 +77,7 @@ class LogoutView(APIView):
 class VerifyMailView(APIView):
     #이메일 인증을 요청하는 경우
     def post(self, request):
+        # TODO: 나중에 아래 검증 로직을 VerifSerializer로 빼기
         email = request.data.get('email')            
         if not email:
             return Response({"error": "이메일 주소가 필요합니다."}, status=status.HTTP_400_BAD_REQUEST)
@@ -82,8 +85,12 @@ class VerifyMailView(APIView):
         if CustomUser.objects.filter(email=email).exists():
             return Response({"error": "이미 가입된 이메일 주소입니다."}, status=status.HTTP_400_BAD_REQUEST)
         #아니라면, 인증번호 생성 후 django.mail을 통해, 인증번호 이메일 발송
+        #인증번호 생성
+        verif_code = str(random.randint(100000, 999999))
         try:
-            send_email(email, '999999')
+            new_verif_request = Verif(email, verif_code)
+            new_verif_request.save()
+            send_email(email, verif_code)
         except:
             return Response({"error": "발송 중 오류가 발생했습니다. 잠시 후 다시 시도 바랍니다."}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
         return Response({"success": "인증번호가 메일로 발송되었습니다."}, status=status.HTTP_200_OK)
