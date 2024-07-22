05a39aec7b57cd35b92ecf36aefb34598b3d44e0
dylan <nyskyline7@gmail.com>
2024-07-22 11:48:33 +0900

    feat: 수정된 2step 회원가입view+model

--- whew-are-you-BE/users/models.py
+++ whew-are-you-BE/users/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from django.core.exceptions import ObjectDoesNotExist
 
 # Create your models here.
 from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
-    username = models.CharField(max_length=30, unique=True)                  #이게 pk가 되어선 안됨.
+    username = models.CharField(max_length=30, unique=True)     #이게 pk가 되어선 안됨.
     email = models.EmailField()
     phone_number = models.CharField(max_length=16)              # 전화번호
     university = models.CharField(max_length=20)                # 대학
@@ -20,4 +21,19 @@ class CustomUser(AbstractUser):
         try:
             return CustomUser.objects.get(username=username)
         except Exception:
+            return None
+        
+class PendingUser(AbstractUser): #하나의 가입 시도당 1개 생김
+    created_at = models.DateTimeField(auto_now_add=True)
+    email = models.EmailField()
+    password = models.CharField(max_length=20)
+    link = models.CharField(max_length=50, null=True) #이메일로 전송되는 링크에의 해시
+    hash = models.CharField(max_length=50, null=True) #프론트로 리다이렉트 될때 사용되는 해시
+    is_active = models.BooleanField(default=True) #이전 가입 시도는 False로 변경
+    
+    @staticmethod
+    def get_pending_or_none_by_username(email):
+        try:
+            return PendingUser.objects.get(email=email)
+        except ObjectDoesNotExist:
             return None
--- whew-are-you-BE/users/views.py
+++ whew-are-you-BE/users/views.py
@@ -80,3 +80,29 @@ class LogoutView(APIView):
     def post(self, request):
         logout(request)
         return Response({"message": "로그아웃되었습니다."}, status=status.HTTP_200_OK)
+    
+class VerifyMailView(APIView):
+    #이메일 인증을 요청하는 경우
+    def post(self, request):
+        email = request.data.get('email')            
+        password = request.data.get('password')
+        if not email or not password:
+            return Response({"error": "이메일 주소와 비밀번호가 필요합니다."}, status=status.HTTP_400_BAD_REQUEST)
+        #이미 회원으로 가입된 메일 주소인지 확인
+        if CustomUser.objects.filter(email=email).exists():
+            return Response({"error": "이미 가입된 이메일 주소입니다."}, status=status.HTTP_400_BAD_REQUEST)
+        #아니라면, 링크 생성
+        #django.mail을 통해, 생성된 링크 이메일 발송
+        #링크를 통해 회원가입 마저 하라고 안내
+        pass
+
+    #인증 링크를 누르는 경우
+    def get(self, request):
+        #해시값이 존재하는지 확인
+        #해시 생성 시간 확인, 유효기간 지났으면 X
+        #is_active값을 False->True로 변경, 이미 True면 X
+        #회원가입을 마저 하기 위한 해시 생성 후 프론트로 리다이렉트
+
+        #추후 프론트는 해시값과 회원가입에 필요한 나머지 값을 RegisterView로 전송하면, CustomUser의 모든 필드 채워진채로 가입 완료
+        #우리는 RegisterView와 이떼 사용하는 Serializer만 손보면 됨.
+        pass
