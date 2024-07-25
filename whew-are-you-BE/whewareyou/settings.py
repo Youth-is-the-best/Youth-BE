7621b48867d2c2303f636cd603a8f98a26e3c3c1
cherrynniii <chaelin20760@gmail.com>
2024-07-25 13:50:29 +0900

    fix: cors_allowed_origins 수정 3001 추가

--- whew-are-you-BE/whewareyou/settings.py
+++ whew-are-you-BE/whewareyou/settings.py
@@ -181,8 +181,8 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [ 
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "https://localhost:3000",
-    "https://127.0.0.1:3000",
+    "https://localhost:3001",
+    "https://127.0.0.1:3001",
     "https://maknaengee.p-e.kr"
 ]
 
