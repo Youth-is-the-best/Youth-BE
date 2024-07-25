84d34ea430bf1f80a9d57227042d4d7d86be6b98
dylan <nyskyline7@gmail.com>
2024-07-25 14:11:41 +0900

    Revert "fix: cors_allowed_origins 수정 슬래시"
    This reverts commit b6da884d84a30e1d9230aa286d8ffac304b8c837.

--- whew-are-you-BE/whewareyou/settings.py
+++ whew-are-you-BE/whewareyou/settings.py
@@ -183,7 +183,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "https://localhost:3001",
     "https://127.0.0.1:3001",
-    "https://maknaengee.p-e.kr/"
+    "https://maknaengee.p-e.kr"
 ]
 
 DATABASES = {
