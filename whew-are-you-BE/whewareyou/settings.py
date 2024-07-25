4dba1ea3c8c66472ccb70b817fa13457d73e27ef
cherrynniii <chaelin20760@gmail.com>
2024-07-25 13:45:05 +0900

    cors_allowed_origins 수정

--- whew-are-you-BE/whewareyou/settings.py
+++ whew-are-you-BE/whewareyou/settings.py
@@ -71,7 +71,7 @@ THIRD_PARTY_APPS = [
 INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS + THIRD_PARTY_APPS
 
 MIDDLEWARE = [
-    #'corsheaders.middleware.CorsMiddleware'
+    'corsheaders.middleware.CorsMiddleware'
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -183,6 +183,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "https://localhost:3000",
     "https://127.0.0.1:3000",
+    "https://maknaengee.p-e.kr"
 ]
 
 DATABASES = {
