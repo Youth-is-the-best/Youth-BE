d93cd184eec03902993891795467caee8481329a
cherrynniii <chaelin20760@gmail.com>
2024-07-26 18:19:57 +0900

    feat: 빙고 칸 모델 추가(추천, 직접입력 모델 나오면 수정 필요)

--- whew-are-you-BE/bingo/models.py
+++ whew-are-you-BE/bingo/models.py
@@ -3,9 +3,22 @@ from django.conf import settings
 
 # 빙고 판 모델
 class Bingo(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True)     # 빙고판의 사용자
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)     # 빙고판의 사용자
     size = models.IntegerField(default=9)       # 빙고의 크기(9, 16)
     start_date = models.DateField()     # 빙고 시작 날짜
     end_date = models.DateField()       # 빙고 종료 날짜
     is_active = models.BooleanField(default=True)       # 빙고 활성화 여부(end_date 이후로는 False가 됨)
     change_chance = models.IntegerChoices(default=3)        # 빙고 수정 남은 기회
+
+
+# 빙고 칸 모델
+class BingoSpace(models.Model):
+    bingo = models.ForeignKey(Bingo, on_delete=models.CASCADE)      # 빙고
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)        # 사용자
+    is_executed = models.BooleanField(default=False)        # 실행 완료 여부
+    start_date = models.DateField()     # 항목 시작 날짜
+    end_date = models.DateField()       # 항목 종료 날짜
+    image = models.ImageField(null=True, blank=True)        # 나중에 후기글의 대표 이미지
+    recommend_content = models.ForeignKey(null=True, blank=True)        # 추천 항목
+    self_content = models.ForeignKey(null=True, blank=True)     # 직접 입력 항목
+    review = models.ForeignKey(null=True, blank=True)       # 후기글
