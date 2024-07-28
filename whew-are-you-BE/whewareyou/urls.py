f0818b6cfcd99c4714214d340853fd1047c64beb
cherrynniii <chaelin20760@gmail.com>
2024-07-29 02:41:46 +0900

    feat: 정보글, 후기글 url 파일 분리

--- whew-are-you-BE/whewareyou/urls.py
+++ whew-are-you-BE/whewareyou/urls.py
@@ -38,5 +38,6 @@ urlpatterns = [
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('bingo/', include('bingo.urls')),
-    path('information/', include('review_information.urls')),
+    path('information/', include('review_information.information_urls')),
+    
 ]
