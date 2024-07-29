3afca5dab8abe8039a025416d09e4b6b03723153
cherrynniii <chaelin20760@gmail.com>
2024-07-29 20:21:51 +0900

    docs: 후기글 모델 정리

--- whew-are-you-BE/review_information/models.py
+++ whew-are-you-BE/review_information/models.py
@@ -3,6 +3,75 @@ from bingo.models import BingoSpace, ToDo
 from users.models import CustomUser
 
 # 후기글 모델
+"""
+후기글 모델은 크게 [빙고 인증용 후기글], [일반 후기글] 두 가지로 분류할 수 있다.
+그리고, 대분류 카테고리에 따라 필요한 필드는 아래와 같다.
+
+1. CAREER(채용, 인턴)
+- user: 사용자
+- title: 제목
+- large_category: 대분류(CAREER)
+- duty: 직무
+- employment_form: 채용 형태
+- area: 근무 지역
+- start_date: 근무 시작 날짜
+- end_date: 근무 종료 날짜
+- procedure: 모집 절차
+- content: 준비 과정/합격 팁/소감
+- images(이미지)
++) todo(인증용 후기글), bingo_space(인증용 후기글), detailplans(일반 후기글)
+
+2. CERTIFICATE(자격증)
+- user: 사용자
+- title: 제목
+- large_category: 대분류(CERTIFICATE)
+- host: 주최사
+- app_fee: 응시료
+- date: 시험 날짜
+- start_date: 준비 시작 날짜
+- end_date: 준비 종료 날짜
+- procedure: 시험 절차
+- content: 준비 과정/공부 팁/소감
+- images(이미지)
++) todo(인증용 후기글), bingo_space(인증용 후기글), detailplans(일반 후기글)
+
+3. CONTEST(공모전)
+- user: 사용자
+- title: 제목
+- large_category: 대분류(CONTEST)
+- host: 주최 기관
+- field: 공모 분야
+- app_due: 마감일
+- start_date: 준비 시작 날짜
+- end_date: 준비 종료 날짜
+- content: 준비 과정/수상 팁/소감
+- images(이미지)
++) todo(인증용 후기글), bingo_space(인증용 후기글), detailplans(일반 후기글)
+
+4. OUTBOUND(대외활동)
+- user: 사용자
+- title: 제목
+- large_category: 대분류(OUTBOUND)
+- field: 활동 분야
+- area: 활동 지역
+- start_date: 활동 시작 날짜
+- end_date: 활동 종료 날짜
+- procedure: 모집 절차
+- content: 활동 내용/합격 팁/소감
+- images(이미지)
++) todo(인증용 후기글), bingo_space(인증용 후기글), detailplans(일반 후기글)
+
+5. 그 외
+- user: 사용자
+- title: 제목
+- large_category: 대분류
+- start_date: 활동 시작 날짜
+- end_date: 활동 종료 날짜
+- content: 소감
+- images(이미지)
++) todo(인증용 후기글), bingo_space(인증용 후기글), detailplans(일반 후기글)
+"""
+
 class Review(models.Model):
 
     BINGO_CATEGORIES = [
@@ -16,24 +85,28 @@ class Review(models.Model):
         ('REST', '휴식')
     ]
 
-    bingo_space = models.OneToOneField(BingoSpace, null=True, blank=True, related_name='review', on_delete=models.CASCADE)      # 빙고 인증용 후기글이면 빙고칸과 연결
+    # 모든 후기글 양식 공통
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='review', blank=True, null=True)
     title = models.CharField(max_length=50)
     large_category = models.CharField(max_length=20, choices=BINGO_CATEGORIES)
-    todo = models.ForeignKey(ToDo, on_delete=models.CASCADE, blank=True, null=True)
     start_date = models.DateField(blank=True, null=True)
     end_date = models.DateField(blank=True, null=True)
     content = models.TextField()
-    duty = models.CharField(max_length=50, null=True)       #직무
-    employment_form = models.CharField(max_length=50, null=True)        #채용형태
-    area = models.CharField(max_length=50, null=True)       #근무/활동지역
-    work_period = models.IntegerField(blank=True, null=True)        # 근무 기간
-    host = models.CharField(max_length=50, null=True) # 주최사
-    app_fee = models.IntegerField(blank=True, null=True)    # 응시료
-    date = models.DateField(blank=True, null=True)        # 시험 날짜
-    app_due = models.DateField(null=True)       # 지원 마감일
-    field = models.CharField(max_length=20, null=True)      # 공모 분야
-    procedure = models.TextField(blank=True, null=True)     # 모집 절차, 시험 절차
+
+    # 빙고 인증용 후기글 공통
+    bingo_space = models.OneToOneField(BingoSpace, null=True, blank=True, related_name='review', on_delete=models.CASCADE)      # 빙고 인증용 후기글
+    todo = models.ForeignKey(ToDo, on_delete=models.CASCADE, blank=True, null=True)     # 빙고 인증용 후기글
+
+    # 분류에 따라 달라지는 양식
+    duty = models.CharField(max_length=50, null=True)       # 채용: 직무
+    employment_form = models.CharField(max_length=50, null=True)        # 채용: 채용 형태
+    area = models.CharField(max_length=50, null=True)       # 대외활동, 채용: 근무/활동지역
+    host = models.CharField(max_length=50, null=True) # 자격증, 공모전: 주최사, 주최 기관
+    app_fee = models.IntegerField(blank=True, null=True)    # 자격증: 응시료
+    date = models.DateField(blank=True, null=True)        # 자격증: 시험 날짜
+    app_due = models.DateField(null=True)       # 공모전: 마감일
+    field = models.CharField(max_length=20, null=True)      # 대외 활동, 공모전: 활동/공모 분야
+    procedure = models.TextField(blank=True, null=True)     # 채용, 자격증, 대외활동: 모집 절차, 시험 절차
 
 
 # 후기글 이미지
