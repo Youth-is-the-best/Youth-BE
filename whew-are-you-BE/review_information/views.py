13165623e2d1951e0b7847642294823e307543ca
cherrynniii <chaelin20760@gmail.com>
2024-07-29 22:06:14 +0900

    feat: 일반 후기글 작성 기능 완성

--- whew-are-you-BE/review_information/views.py
+++ whew-are-you-BE/review_information/views.py
@@ -1,11 +1,12 @@
 from rest_framework.views import APIView
 from .permissions import IsAdminOrReadOnly
-from .models import Information, InformationImage
+from .models import Information, InformationImage, Review, ReviewImage
 from .serializers import *
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.parsers import MultiPartParser, FormParser, JSONParser
 from django.shortcuts import get_object_or_404
+import json
 
 # 모든 정보글 뷰
 class InformationAPIView(APIView):
@@ -42,11 +43,23 @@ class ReviewAPIView(APIView):
     parser_classes = [MultiPartParser, FormParser, JSONParser]
 
     def post(self, request, *args, **kwargs):
-        serializer = ReviewPOSTSerializer(data=request.data, context={'request':request})
+        json_data = request.data.get('json')
+        if json_data:
+            data = json.loads(json_data)
+        else:
+            data = {}
+
+        serializer = ReviewPOSTSerializer(data=data, context={'request':request})
         
         if serializer.is_valid():
-            serializer.save(user=request.user)
+            review = serializer.save()
+            if 'images' in request.FILES:
+                images = request.FILES.getlist('images')
+                for image in images:
+                    ReviewImage.objects.create(review=review, image=image)
+                    
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         print(serializer.errors)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
