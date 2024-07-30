5a4960dd20e5fea37fc19bada3d8b9fcf84878a5
cherrynniii <chaelin20760@gmail.com>
2024-07-30 14:47:39 +0900

    feat: 빙고 인증용 후기글 수정 전

--- whew-are-you-BE/bingo/serializers.py
+++ whew-are-you-BE/bingo/serializers.py
@@ -34,32 +34,35 @@ class ReviewPOSTSerializer(serializers.ModelSerializer):
         fields = ["location", "procedure", "content", "large_category"]
 
     def create(self, validated_data):
-        large_category = validated_data['large_category']
-        location = int(validated_data['location'])
-        procedure = validated_data['procedure']
+        # 사용자가 입력
+        large_category = validated_data['large_category']       # 분류
+        location = int(validated_data['location'])      # 빙고 칸의 위치(0~8)
         content = validated_data['content']
-        images = self.context['request'].FILES.getlist('images')
+        procedure = validated_data.get('procedure')     # 채용, 자격증, 대외 활동: 모집/시험 절차
+        images = self.context['request'].FILES.getlist('images')        # 사용자가 올린 이미지
+
+        # 자동 입력
         user = self.context['user'].user
         bingo = Bingo.objects.get(user=user, is_activate=True)
         bingo_space = BingoSpace.objects.get(bingo=bingo, location=location)
         todo = bingo_space.todo.all()
-        date = bingo_space.date     # 시험 날짜
+        date = bingo_space.date     # 자격증: 시험 날짜
         start_date_user = bingo_space.start_date
         end_date_user = bingo_space.end_date
 
         # 끌어오기 항목의 후기글인 경우
         if bingo_space.recommend_content:
-            title = bingo_space.recommend_content.title
-            duty = bingo_space.recommend_content.duty
-            employment_form = bingo_space.recommend_content.employment_form
-            area = bingo_space.recommend_content.area
-            start_date = bingo_space.recommend_content.start_date
-            end_date = bingo_space.recommend_content.end_date
-            host = bingo_space.recommend_content.host
-            app_fee = bingo_space.recommend_content.app_fee
-            prep_period = bingo_space.recommend_content.prep_period
-            app_due = bingo_space.recommend_content.app_due
-            field = bingo_space.recommend_content.field
+            title = bingo_space.recommend_content.title         # 제목
+            duty = bingo_space.recommend_content.duty       # 채용: 직무
+            employment_form = bingo_space.recommend_content.employment_form     # 채용: 채용 형태
+            area = bingo_space.recommend_content.area       # 채용: 근무 지역, 대외활동: 활동 지역
+            start_date = bingo_space.recommend_content.start_date       # 행사 시작 날짜
+            end_date = bingo_space.recommend_content.end_date       # 행사 종료 날짜
+            host = bingo_space.recommend_content.host       # 자격증, 공모전: 주최 기관
+            app_fee = bingo_space.recommend_content.app_fee     # 자격증: 응시료
+            prep_period = bingo_space.recommend_content.prep_period     # 준비 기간
+            app_due = bingo_space.recommend_content.app_due         # 공모전: 마감일
+            field = bingo_space.recommend_content.field     # 공모전, 대외활동: 분야
 
         # 직접 작성한 항목의 후기글인 경우
         elif bingo_space.self_content:
@@ -77,24 +80,30 @@ class ReviewPOSTSerializer(serializers.ModelSerializer):
 
         # 인턴(채용) 카테고리인 경우
         if large_category == 'CAREER':
-            review = Review(user=user, title=title, large_category=large_category, duty=duty, employment_form=employment_form, area=area, start_date=start_date, end_date=end_date, bingo_space=bingo_space, content=content, todo=todo, procedure=procedure)
+            review = Review(user=user, large_category=large_category, content=content, title=title, start_date=start_date_user, end_date=end_date_user,
+                            duty=duty, employment_form=employment_form, area=area, procedure=procedure, bingo_space=bingo_space)
         # 자격증 카테고리인 경우
         elif large_category == 'CERTIFICATE':
-            review = Review(user=user, bingo_space=bingo_space, title=title, large_category=large_category, host=host, app_fee=app_fee, date=date, start_date=start_date_user, end_date=end_date_user, todo=todo, procedure=procedure, content=content)
+            review = Review(user=user, title=title, large_category=large_category, host=host, app_fee=app_fee, date=date, bingo_space=bingo_space,
+                            start_date=start_date_user, end_date=end_date_user, procedure=procedure, content=content)
         # 대외 활동 카테고리인 경우
         elif large_category == 'OUTBOUND':
-            review = Review(user=user, bingo_space=bingo_space, title=title, large_category=large_category, start_date=start_date_user, end_date=end_date_user, field=field, area=area, todo=todo, procedure=procedure, content=content)
+            review = Review(user=user, title=title, large_category=large_category, field=field, area=area, start_date=start_date_user, end_date=end_date_user,
+                            procedure=procedure, content=content, bingo_space=bingo_space)
         # 공모전 카테고리인 경우
         elif large_category == 'CONTEST':
-            review = Review(user=user, bingo_space=bingo_space, title=title, large_category=large_category, host=host, field=field, date=date, start_date=start_date_user, end_date=end_date_user, todo=todo, procedure=procedure, content=content)
+            review = Review(user=user, title=title, large_category=large_category, host=host, field=field, 
+                            app_due=app_due, start_date=start_date_user, end_date=end_date_user, content=content, bingo_space=bingo_space)
         # 그 외(취미, 여행, 자기 계발, 휴식)
         elif large_category in ['HOBBY', 'TRAVEL', 'SELFIMPROVEMENT', 'REST']:
-            review = Review(bingo_space=bingo_space, title=title, large_category=large_category, start_date=start_date, end_date=end_date, todo=todo, content=content)
+            review = Review(user=user, title=title, large_category=large_category, start_date=start_date_user, end_date=end_date_user, 
+                            content=content, bingo_space=bingo_space)
         # 잘못 입력
         else:
             return Response({"error": "요청한 카테고리 항목이 존재하지 않습니다."}, status=status.HTTP_400_BAD_REQUEST)
 
         review.save()
+
         bingo_space.is_executed = True      # 빙고 완료 표시
         bingo_space.save()
 
@@ -102,4 +111,5 @@ class ReviewPOSTSerializer(serializers.ModelSerializer):
             image_data = ReviewImage(review=review, image=image)
             image_data.save()
         validated_data['images'] = images
-        return validated_data
+        
+        return review
