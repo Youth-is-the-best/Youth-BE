dde20f62f9ab49f535f32ae3ced4e6044bcfb1bb
dylan <nyskyline7@gmail.com>
2024-08-02 10:49:44 +0900

    feat: 투두 상태 변경용 엔드포인트 추가

--- whew-are-you-BE/bingo/urls.py
+++ whew-are-you-BE/bingo/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('recs/', BingoRecsAPIView.as_view()), #유형별 추천
     path('items/<int:pk>/', BingoItemAPIView.as_view()),
     path('dday/', DdayAPIView.as_view()),      # 디데이를 설정하거나 가져오기
+    path('todo/<int:todo_id>', ToDoAPIView.as_view()) #투두 체크박스 수정용 엔드포인트
 ]
