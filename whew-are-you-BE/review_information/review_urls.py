daf1576111951cbd6844852b954b5d99db1e1e9a
dylan <nyskyline7@gmail.com>
2024-08-02 09:43:20 +0900

    fix: 단순 오타 수정

--- whew-are-you-BE/review_information/review_urls.py
+++ whew-are-you-BE/review_information/review_urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('likes/<int:id>/', ReviewLikeAPIView.as_view()),
     path('storages/<int:id>/', ReviewStorageAPIView.as_view()),
     path('<int:review_id>/comments/', CommentAPIView.as_view()),
-    path('related/<int:bingo_item_id>/', FetchRelatedReviewsAPIView.as_view())
+    path('related/<int:bingo_item_id>/', FetchRelatedReviewsAPIView.as_view()),
     path('comments/<int:comment_id>/', CommentDetailAPIView.as_view()),
 ]
