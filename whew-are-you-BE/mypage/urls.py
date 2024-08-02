ccb74399d4076b6cf8f17c50d32bc1d0c4537e85
cherrynniii <chaelin20760@gmail.com>
2024-08-02 13:17:47 +0900

    feat: 알림 기능 완성

--- whew-are-you-BE/mypage/urls.py
+++ whew-are-you-BE/mypage/urls.py
@@ -3,5 +3,10 @@ from .views import *
 
 urlpatterns = [
     path('', MyInfoAPIView.as_view()),    
-    path('news/', NewsAPIView.as_view())
+    path('news/', NewsAPIView.as_view()),
+    path('news/option/heart/', HeartOptionAPIView.as_view()),
+    path('news/option/comment/', CommentOptionAPIView.as_view()),
+    path('news/option/hue/', HueOptionAPIView.as_view()),
+    path('news/option/point/', PointOptionAPIView.as_view()),
+    path('news/option/not-read/', ReadOptionAPIView.as_view()),
 ]
