a7ede85bce4f5d4b720d42307c2092de982bf1b0
cherrynniii <chaelin20760@gmail.com>
2024-08-04 19:28:32 +0900

    feat: 후기글/공고 작성 시간 수정

--- whew-are-you-BE/review_information/models.py
+++ whew-are-you-BE/review_information/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from bingo.models import BingoSpace, ToDo
 from users.models import CustomUser
+from datetime import date
 
 # 후기글 모델
 """
@@ -92,7 +93,7 @@ class Review(models.Model):
     start_date = models.DateField()
     end_date = models.DateField()
     content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateField(editable=False)
 
     # 빙고 인증용 후기글 공통
     bingo_space = models.OneToOneField(BingoSpace, null=True, blank=True, related_name='review', on_delete=models.CASCADE)      # 빙고 인증용 후기글
@@ -111,6 +112,11 @@ class Review(models.Model):
     # 좋아요, 보관함
     likes = models.ManyToManyField(CustomUser, related_name='like_review', blank=True)
     storage = models.ManyToManyField(CustomUser, related_name='storage_review', blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.id:  # 객체가 처음 생성될 때만 설정
+            self.created_date = date.today()
+        super(Review, self).save(*args, **kwargs)
 
     def likes_count(self):
         return self.likes.count()
