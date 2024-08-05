bd3ea8a4b1a21754487f4482047ee1f2f6b1c2a3
dylan <nyskyline7@gmail.com>
2024-08-05 12:07:06 +0900

    feat: 유형 국문으로 변환 제공

--- whew-are-you-BE/mypage/views.py
+++ whew-are-you-BE/mypage/views.py
@@ -18,6 +18,21 @@ class MyInfoAPIView(APIView):
         email = logged_in_user.email
         points = "1000"        
 
+        if type_result == 'SQUIRREL':
+            type_result = '준비성 철저한 다람쥐'
+        elif type_result == 'RABBIT':
+            type_result = '열정 가득 부지런한 토끼'
+        elif type_result == 'PANDA':
+            type_result = '재충전을 원하는 판다'
+        elif type_result == 'BEAVER':
+            type_result = '끝없는 발전을 추구하는 비버'
+        elif type_result == 'EAGLE':
+            type_result = '모험을 갈망하는 독수리'
+        elif type_result == 'BEAR':
+            type_result = '안정을 추구하는 곰'
+        elif type_result == 'DOLPHIN':
+            type_result = '호기심 많은 돌고래'
+
         return Response({
             "name": name,
             "username": username,
